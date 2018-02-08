39f80806c1dde0c787ed2e10b7c97f4e9a9612c1
ruhidamda <ruhidamda@gmail.com>
2018-02-07 20:00:58 -0600

    Added Mutual Funds Portfolio

--- portfolio/models.py
+++ portfolio/models.py
@@ -68,3 +68,19 @@ class Stock(models.Model):
     def initial_stock_value(self):
         return self.shares * self.purchase_price
 
+class MutualFund(models.Model):
+    customer = models.ForeignKey(Customer, related_name='mutualfunds')
+    type = models.CharField(max_length=50)
+    share_class = models.CharField(max_length=50)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField(default=timezone.now, blank=True, null=True)
+
+    def created(self):
+        self.recent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.customer)
+
+    def initial_mutualfund_value(self):
+        return self.shares * self.purchase_price
--- portfolio/views.py
+++ portfolio/views.py
@@ -15,6 +15,21 @@ def customer_list(request):
    return render(request, 'portfolio/customer_list.html',{'customers': customer})
 
 @login_required
+def customer_new(request):
+   if request.method == "POST":
+       form = CustomerForm(request.POST)
+       if form.is_valid():
+           customer = form.save(commit=False)
+           customer.created_date = timezone.now()
+           customer.save()
+           customers = Customer.objects.filter(created_date__lte=timezone.now())
+           return render(request, 'portfolio/customer_list.html', {'customers': customers})
+   else:
+       form = CustomerForm()
+       # print("Else")
+   return render(request, 'portfolio/customer_new.html', {'form': form})
+
+@login_required
 def customer_edit(request, pk):
    customer = get_object_or_404(Customer, pk=pk)
    if request.method == "POST":
@@ -135,6 +150,53 @@ def investment_delete(request, pk):
 
 
 @login_required
+def mutualfund_list(request):
+    mutualfunds = MutualFund.objects.filter(purchase_date__lte=timezone.now())
+    return render(request, 'portfolio/mutualfund_list.html', {'mutualfunds': mutualfunds})
+
+
+@login_required
+def mutualfund_new(request):
+    if request.method == "POST":
+        form = MutualFundForm(request.POST)
+        if form.is_valid():
+            mutualfund = form.save(commit=False)
+            mutualfund.created_date = timezone.now()
+            mutualfund.save()
+            mutualfunds = MutualFund.objects.filter(purchase_date__lte=timezone.now())
+            return render(request, 'portfolio/mutualfund_list.html', {'mutualfunds': mutualfunds})
+    else:
+        form = MutualFundForm()
+        # print("Else")
+    return render(request, 'portfolio/mutualfund_new.html', {'form': form})
+
+
+@login_required
+def mutualfund_edit(request, pk):
+    mutualfund = get_object_or_404(MutualFund, pk=pk)
+    if request.method == "POST":
+        form = MutualFundForm(request.POST, instance=mutualfund)
+        if form.is_valid():
+            mutualfund = form.save()
+            # mutualfund.customer = mutualfund.id
+            mutualfund.updated_date = timezone.now()
+            mutualfund.save()
+            mutualfunds = MutualFund.objects.filter(purchase_date__lte=timezone.now())
+            return render(request, 'portfolio/mutualfund_list.html', {'mutualfunds': mutualfunds})
+    else:
+       # print("else")
+       form = MutualFundForm(instance=mutualfund)
+    return render(request, 'portfolio/mutualfund_edit.html', {'form': form})
+
+@login_required
+def mutualfund_delete(request, pk):
+    mutualfund = get_object_or_404(MutualFund, pk=pk)
+    mutualfund.delete()
+    mutualfunds = MutualFund.objects.filter(purchase_date__lte=timezone.now())
+    return render(request, 'portfolio/mutualfund_list.html', {'mutualfunds': mutualfunds})
+
+
+@login_required
 def portfolio(request,pk):
    customer = get_object_or_404(Customer, pk=pk)
    customers = Customer.objects.filter(created_date__lte=timezone.now())
@@ -144,6 +206,6 @@ def portfolio(request,pk):
 
 
    return render(request, 'portfolio/portfolio.html', {'customers': customers, 'investments': investments,
-                                                      'stocks': stocks,
+                                                      'stocks': stocks, 'mutualfunds': mutualfunds,
                                                       'sum_acquired_value': sum_acquired_value,})
 
